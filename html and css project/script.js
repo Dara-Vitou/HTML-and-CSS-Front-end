8948ab39921779d253e9dc00442cfe5706a5035e
CHAOBAN DARAVITOU <156092045+coum-man@users.noreply.github.com>
2024-06-06 04:39:51 +0700

    Add files via upload

---
+++ html and css project/script.js
@@ -0,0 +1,17 @@
+function redirect() {
+    addEventListener('click', () => {
+        window.location.href = 'shop.html';
+    });
+}
+
+function toPay() {
+    addEventListener('click', () => {
+        window.location.href = 'payment.html';
+    });
+}
+
+function toChange() {
+    addEventListener('click', () => {
+        window.location.href = 'change-password.html';
+    });
+}
